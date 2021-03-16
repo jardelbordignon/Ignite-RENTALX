4d41bcc4f2a174c0d6486edef090f1bb3919ea65
Jardel <jardel1101@gmail.com>
2021-03-15 23:40:01 -0300

    Create categories

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
+import { routes } from './routes'
 
-app.use(express.json());
+const app = express()
 
-const port = 3333;
-app.listen(port, () => console.log(`Server running - port ${port}`));
+app.use(express.json())
+app.use(routes)
+
+const port = 3333
+app.listen(port, () => console.log(`Server running - port ${port}`))
