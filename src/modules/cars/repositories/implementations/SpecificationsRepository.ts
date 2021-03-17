c96836b5fd30c8085c39fb55d0c01ec070e20358
Jardel <jardel1101@gmail.com>
2021-03-17 16:39:32 -0300

    Separating repository implementations

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,8 +1,8 @@
-import Specification from '../model/Specification'
+import Specification from '../../model/Specification'
 import {
   ISpecificationsRepository,
   ICreateSpecificationDTO,
-} from './ISpecificationsRepository'
+} from '../ISpecificationsRepository'
 
 export default class SpecificationsRepository
   implements ISpecificationsRepository {
