aaddb4634cffed61fca383f9acb8a321f9afd984
Jardel <jardel1101@gmail.com>
2021-03-18 00:06:31 -0300

    Add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
 
 import { routes } from './routes'
+import swaggerJson from './swagger.json'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson))
+
 app.use(routes)
 
 const port = 3333
