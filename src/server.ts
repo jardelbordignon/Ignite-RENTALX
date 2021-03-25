b4978212b69134b1b434ebc99b548c98560131c7
Jardel <jardel1101@gmail.com>
2021-03-25 14:47:46 -0300

    Criando container do postgres

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import swaggerUi from 'swagger-ui-express'
 import { routes } from './routes'
 import swaggerJson from './swagger.json'
 
+import './database'
+
 const app = express()
 
 app.use(express.json())
