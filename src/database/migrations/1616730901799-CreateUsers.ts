288a8e28772740bfb5ec391b6b9cc8c287d991e0
Jardel <jardel1101@gmail.com>
2021-03-26 01:54:02 -0300

    Init accounts module

---
+++ src/database/migrations/1616730901799-CreateUsers.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export default class CreateUsers1616730901799 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar(50)',
+          },
+          {
+            name: 'username',
+            type: 'varchar(50)',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar(100)',
+          },
+          {
+            name: 'driver_license',
+            type: 'varchar(40)',
+          },
+          {
+            name: 'isAdmin',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,24 @@
+import { Column, Entity } from 'typeorm'
+
+import Default from '../../../shared/entities/Default'
+
+@Entity('users')
+export class User extends Default {
+  @Column()
+  name: string
+
+  @Column()
+  username: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @Column()
+  driver_license: string
+
+  @Column()
+  isAdmin: boolean
+}
