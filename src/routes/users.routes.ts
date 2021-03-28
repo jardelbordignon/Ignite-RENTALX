cec7e5700307acbb8e0bf77c247b9f5dedc9e070
Jardel <jardel1101@gmail.com>
2021-03-28 11:06:32 -0300

    Avatar file upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,25 @@
 import { Router } from 'express'
+import multer from 'multer'
 
+import configUpload from '../config/upload'
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController'
 import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController'
 
 const usersRoutes = Router()
+const uploadAvatar = multer(configUpload.upload('./tmp/avatar'))
 
 const createUserController = new CreateUserController()
 const updateUserAvatarController = new UpdateUserAvatarController()
 
 usersRoutes.post('/', createUserController.handle)
-usersRoutes.patch('/avatar', updateUserAvatarController.handle)
+
+usersRoutes.use(ensureAuthenticated)
+
+usersRoutes.patch(
+  '/avatar',
+  uploadAvatar.single('avatar'),
+  updateUserAvatarController.handle
+)
 
 export { usersRoutes }
