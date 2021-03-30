ddf99c9ea1fa9b87d32d3237b20588a04851ad89
Jardel <jardel1101@gmail.com>
2021-03-30 02:03:17 -0300

    CreatecategoryUseCase Test

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,25 @@
+import { v4 as uuid } from 'uuid'
+
+import Category from '../../entities/Category'
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from '../ICategoriesRepository'
+
+export class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = []
+
+  async list(): Promise<Category[]> {
+    return this.categories
+  }
+
+  async findByName(name: string): Promise<Category> {
+    return this.categories.find((category) => category.name === name)
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category()
+    Object.assign(category, { id: uuid(), name, description })
+    this.categories.push(category)
+  }
+}
---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,43 @@
+import { AppError } from '../../../../errors/AppError'
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory'
+import { CreateCategoryUseCase } from './CreateCategoryUseCase'
+
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
+let createCategoryUseCase: CreateCategoryUseCase
+
+describe('CreateCategoryUseCase', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory()
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    )
+  })
+
+  it('Should be able to create a new category', async () => {
+    const category = {
+      name: 'Category name test',
+      description: 'Category description test',
+    }
+
+    await createCategoryUseCase.execute(category)
+
+    const createdCategory = await categoriesRepositoryInMemory.findByName(
+      category.name
+    )
+
+    expect(createdCategory).toHaveProperty('id')
+  })
+
+  it('Should not be able to create a new category with exists name', async () => {
+    const category = {
+      name: 'Category name test',
+      description: 'Category description test',
+    }
+
+    await createCategoryUseCase.execute(category)
+
+    await expect(
+      createCategoryUseCase.execute(category)
+    ).rejects.toBeInstanceOf(AppError)
+  })
+})
