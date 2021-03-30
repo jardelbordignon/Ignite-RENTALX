99839cc21551e4f1b689bd2d1f36ddec015be4a4
Jardel <jardel1101@gmail.com>
2021-03-30 13:41:00 -0300

    Imports with @

--- src/modules/cars/useCases/createSpecifications/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecifications/CreateSpecificationUseCase.ts
@@ -1,10 +1,10 @@
 import { inject, injectable } from 'tsyringe'
 
-import { AppError } from '../../../../errors/AppError'
+import { AppError } from '@/errors/AppError'
 import {
   ICreateSpecificationDTO,
   ISpecificationsRepository,
-} from '../../repositories/ISpecificationsRepository'
+} from '@/modules/cars/repositories/ISpecificationsRepository'
 
 @injectable()
 export class CreateSpecificationUseCase {
