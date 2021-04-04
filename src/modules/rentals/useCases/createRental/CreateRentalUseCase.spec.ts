cdb25594bc2181f752ed1014485b429f04ae4100
Jardel <jardel1101@gmail.com>
2021-04-04 18:47:30 -0300

    Dates with dayjs lib

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,3 +1,7 @@
+import dayjs from 'dayjs'
+
+import { AppError } from '@/shared/errors/AppError'
+
 import { RentalsRepositoryInMemory } from '../../repositories/in-memory/RentalsRepositoryInMemory'
 import { CreateRentalUseCase } from './CreateRentalUseCase'
 
@@ -5,6 +9,8 @@ let createRentalUseCase: CreateRentalUseCase
 let rentalRepositoryInMemory: RentalsRepositoryInMemory
 
 describe('CreateRentalUseCase', () => {
+  const expected_return_date = dayjs().add(1, 'day').toDate()
+
   beforeEach(() => {
     rentalRepositoryInMemory = new RentalsRepositoryInMemory()
     createRentalUseCase = new CreateRentalUseCase(rentalRepositoryInMemory)
@@ -14,11 +20,54 @@ describe('CreateRentalUseCase', () => {
     const rental = await createRentalUseCase.execute({
       user_id: 'uuid-user123',
       car_id: 'uuid-car123',
-      expected_return_date: new Date(),
+      expected_return_date,
     })
 
     console.log(rental)
 
     expect(rental).toHaveProperty('id')
+    expect(rental).toHaveProperty('start_date')
+  })
+
+  it('should not be able to create a new rental if there is another opened to the same user', async () => {
+    await createRentalUseCase.execute({
+      user_id: 'uuid-user123',
+      car_id: 'uuid-car123',
+      expected_return_date,
+    })
+
+    await expect(
+      createRentalUseCase.execute({
+        user_id: 'uuid-user123',
+        car_id: 'uuid-car124',
+        expected_return_date,
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should not be able to create a new rental if there is another opened to the same car', async () => {
+    await createRentalUseCase.execute({
+      user_id: 'uuid-user123',
+      car_id: 'uuid-car123',
+      expected_return_date,
+    })
+
+    await expect(
+      createRentalUseCase.execute({
+        user_id: 'uuid-user124',
+        car_id: 'uuid-car123',
+        expected_return_date,
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should not be able to create a new rental whithout minimun rental hours', async () => {
+    expect(async () => {
+      createRentalUseCase.execute({
+        user_id: 'uuid-user124',
+        car_id: 'uuid-car123',
+        expected_return_date: dayjs().toDate(),
+      })
+    }).rejects.toBeInstanceOf(AppError)
   })
 })
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,7 +1,12 @@
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+
 import { AppError } from '@/shared/errors/AppError'
 
 import { Rental } from '../../infra/typeorm/entities/Rental'
 import { IRentalsRepository } from '../../repositories/IRentalsRepository'
+
+dayjs.extend(utc)
 
 interface IRequest {
   user_id: string
@@ -12,6 +17,8 @@ interface IRequest {
 export class CreateRentalUseCase {
   constructor(private rentalsRepository: IRentalsRepository) {}
   async execute(data: IRequest): Promise<Rental> {
+    const minRentalHours = 24
+
     const openedRentalByCar = await this.rentalsRepository.findOpenedRentalByCar(
       data.car_id
     )
@@ -23,6 +30,18 @@ export class CreateRentalUseCase {
     )
 
     if (openedRentalByUser) throw new AppError("There's a rental in progress")
+
+    const expected_return_date_utc = dayjs(data.expected_return_date)
+      .utc()
+      .local()
+      .format()
+
+    const date_now_utc = dayjs().utc().local().format()
+
+    const compare = dayjs(expected_return_date_utc).diff(date_now_utc, 'hours')
+
+    if (compare < minRentalHours)
+      throw new AppError(`Minimun rental hours must be ${minRentalHours}`)
 
     const rental = await this.rentalsRepository.save(data)
 
