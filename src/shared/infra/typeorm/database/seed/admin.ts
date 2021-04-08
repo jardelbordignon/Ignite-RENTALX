1f60a3bd31cabab78f2280586f9e9c172511bd70
Jardel <jardel1101@gmail.com>
2021-04-08 14:18:56 -0300

    Upgrade swagger documentation

--- src/shared/infra/typeorm/database/seed/admin.ts
+++ src/shared/infra/typeorm/database/seed/admin.ts
@@ -6,12 +6,17 @@ import createConnection from '../index'
 async function create() {
   const connection = await createConnection('localhost')
 
-  const id = uuid()
-  const password = await hash('admin', 8)
+  const pwd_admin = await hash('admin', 8)
+  const pwd_user = await hash('1234', 8)
 
   await connection.query(
     `INSERT INTO users(id, name, email, password, driver_license, is_admin, created_at, updated_at)
-    VALUES('${id}', 'admin', 'admin@email.com', '${password}', '12345678910', true, 'now()', 'now()')
+    VALUES('${uuid()}', 'admin', 'admin@email.com', '${pwd_admin}', '12345678910', true, 'now()', 'now()')
+    `
+  )
+  await connection.query(
+    `INSERT INTO users(id, name, email, password, driver_license, is_admin, created_at, updated_at)
+    VALUES('${uuid()}', 'User', 'user@email.com', '${pwd_user}', '12345678911', false, 'now()', 'now()')
     `
   )
 }
