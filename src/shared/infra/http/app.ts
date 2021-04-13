88d5b8ca163fe85f3de936a6e1539da2b330df90
Jardel <jardel1101@gmail.com>
2021-04-13 06:15:37 -0300

    Local files access

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,7 @@ import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import swaggerUi from 'swagger-ui-express'
 
+import upload from '@/config/upload'
 import { AppError } from '@/shared/errors/AppError'
 import { routes } from '@/shared/infra/http/routes'
 import createConnection from '@/shared/infra/typeorm/database'
@@ -16,6 +17,9 @@ const app = express()
 app.use(express.json())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson))
+
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`))
+app.use('/car', express.static(`${upload.tmpFolder}/car`))
 
 app.use(routes)
 
