a15e6983739b3e2f0a118055194f90bb41ac18c3
Jardel <jardel1101@gmail.com>
2021-04-12 21:51:21 -0300

    Storage provider

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -7,7 +7,7 @@ import { UpdateUserAvatarController } from '@/modules/accounts/useCases/updateUs
 import { ensureAuthenticated } from '@/shared/infra/http/middlewares/ensureAuthenticated'
 
 const usersRoutes = Router()
-const uploadAvatar = multer(configUpload.upload('./tmp/avatar'))
+const uploadAvatar = multer(configUpload)
 
 const createUserController = new CreateUserController()
 const updateUserAvatarController = new UpdateUserAvatarController()
